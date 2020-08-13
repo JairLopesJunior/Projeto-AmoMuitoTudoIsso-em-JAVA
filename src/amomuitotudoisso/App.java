77e398707711f427adc665aaf3aa666de36518f3
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2020-08-13 01:32:28 -0300

    Criado 2 filas utilizando a coletions DEQUE do JAVA

--- src/amomuitotudoisso/App.java
+++ src/amomuitotudoisso/App.java
@@ -17,32 +17,46 @@ public class App {
 			System.out.println("==============================");
 			System.out.println("2. Encontrar Cliente");
 			System.out.println("==============================");
+			System.out.println("3. Encontrar Cliente Preferencial");
+			System.out.println("==============================");
 			System.out.println("0. Sair");
 			System.out.println("==============================");
 			opcao = scan.nextInt();
 			
 			if(opcao == 1) {
 				Cliente c = new Cliente();
+				ClienteIdoso ci = new ClienteIdoso();
 				System.out.print("Informe o nome do Cliente:");
 				String nome = scan.next();
-				c.setNome(nome);
 				System.out.print("Informe a idade do Cliente:");
 				int idade = scan.nextInt();
-				c.setIdade(idade);
-				sc.verificarIdade(idade);
 				System.out.print("Informe o CPF do Cliente:");
 				String cpf = scan.next();
-				c.setCpf(cpf);
 				Cliente clienteEncontrado = sc.encontrarClienteCpf(cpf);
-				if(clienteEncontrado != null) {
+				ClienteIdoso clienteIdosoEncontrado = sc.encontrarClienteIdosoCpf(cpf);
+				if(clienteEncontrado != null || clienteIdosoEncontrado != null) {
 					System.out.println("\n==============================");
 					System.out.println("Cliente já cadastrado.");
 					System.out.println("==============================\n");
 				}else {
-					sc.add(c);
-					System.out.println("\n==============================");
-					System.out.println("Cliente cadastrado com sucesso.");
-					System.out.println("==============================\n");
+					boolean clienteIdoso = sc.verificarIdade(idade);
+					if(clienteIdoso == true) {
+						ci.setNome(nome);
+						ci.setIdade(idade);
+						ci.setCpf(cpf);
+						sc.addIdoso(ci);
+						System.out.println("\n==============================");
+						System.out.println("Cliente idoso adicionado.");
+						System.out.println("==============================\n");
+					}else {
+						c.setNome(nome);
+						c.setIdade(idade);
+						c.setCpf(cpf);
+						sc.add(c);
+						System.out.println("\n==============================");
+						System.out.println("Cliente cadastrado com sucesso.");
+						System.out.println("==============================\n");
+					}
 				}
 			}
 			
@@ -56,6 +70,27 @@ public class App {
 					System.out.println("Nome: " + encontrarCliente.getNome());
 					System.out.println("Idade: " + encontrarCliente.getIdade());
 					int encontrar = sc.verificarIndex(encontrarCliente, count);
+					if(encontrar != -1) {
+						System.out.println("Posição na fila: " + (encontrar+1));
+						System.out.println("==============================\n");
+					}
+				}else {
+					System.out.println("\n==============================");
+					System.out.println("Cliente não encontrado.");
+					System.out.println("==============================\n");
+				}
+			}
+			
+			if(opcao == 3) {
+				int count = 0;
+				System.out.println("Informe o nome que deseja encontrar:");
+				String nome = scan.next();
+				ClienteIdoso clienteIdosoEncontrado = sc.encontrarClienteIdoso(nome);
+				if (clienteIdosoEncontrado != null) {
+					System.out.println("\n==============================");
+					System.out.println("Nome: " + clienteIdosoEncontrado.getNome());
+					System.out.println("Idade: " + clienteIdosoEncontrado.getIdade());
+					int encontrar = sc.verificarIndexIdoso(clienteIdosoEncontrado, count);
 					if(encontrar != -1) {
 						System.out.println("Posição na fila: " + (encontrar+1));
 						System.out.println("==============================\n");
--- src/amomuitotudoisso/FilaClientes.java
+++ src/amomuitotudoisso/FilaClientes.java
@@ -1,16 +1,21 @@
 package amomuitotudoisso;
 
+import java.util.Deque;
 import java.util.LinkedList;
-import java.util.Queue;
 
 public class FilaClientes {
 
-	Queue<Cliente> fila = new LinkedList<Cliente>();
+	Deque<Cliente> fila = new LinkedList<Cliente>();
+	Deque<ClienteIdoso> filaIdosa = new LinkedList<ClienteIdoso>();
 	
 	public void add(Cliente clientes) {
 		fila.add(clientes);
 	}
-
+	
+	public void addIdoso(ClienteIdoso clienteIdoso) {
+		filaIdosa.addLast(clienteIdoso);
+	}
+	
 	public int verificarIndex(Cliente clientes, int count) {
 		for(Cliente c : fila) {
 			if(c.getCpf().equals(clientes.getCpf())) {
@@ -20,17 +25,28 @@ public class FilaClientes {
 		}
 		return -1;
 	}
-	
-	public void verificarIdade(int idade, Cliente cliente) {
-		if(idade > 65) {
-			fila.add(cliente);
+
+	public int verificarIndexIdoso(ClienteIdoso clientes, int count) {
+		for(ClienteIdoso c : filaIdosa) {
+			if(c.getCpf().equals(clientes.getCpf())) {
+				return count;
+			}
+			count++;
 		}
+		return -1;
 	}
 	
-	public Cliente encontrarClienteCpf(String cpf) {
+	public boolean verificarIdade(int idade) {
+		if(idade > 65) {
+			return true;
+		}
+		return false;
+	}
+
+	public Cliente verificarClientesIdosos() {
 		for(Cliente c : fila) {
-			if(c.getCpf().equals(cpf)) {
-				return c;
+			if(c.getIdade() > 65) {
+				fila.pop();
 			}
 		}
 		return null;
@@ -45,12 +61,40 @@ public class FilaClientes {
 		}
 		return null;
 	}
+	
+	public ClienteIdoso encontrarClienteIdoso(String nome) {
+		for(ClienteIdoso cl : filaIdosa) {
+			String nomeCerto = cl.getNome();
+			if(nomeCerto.equals(nome)) {
+				return cl;
+			}
+		}
+		return null;
+	}
+	
+	public ClienteIdoso encontrarClienteIdosoCpf(String cpf) {
+		for(ClienteIdoso cl : filaIdosa) {
+			if(cl.getCpf().equals(cpf)) {
+				return cl;
+			}
+		}
+		return null;
+	}
+	
+	public Cliente encontrarClienteCpf(String cpf) {
+		for(Cliente cl : fila) {
+			if(cl.getCpf().equals(cpf)) {
+				return cl;
+			}
+		}
+		return null;
+	}
 
-	public Queue<Cliente> getFila() {
+	public Deque<Cliente> getFila() {
 		return fila;
 	}
 
-	public void setFila(Queue<Cliente> fila) {
+	public void setFila(Deque<Cliente> fila) {
 		this.fila = fila;
 	}
 	
